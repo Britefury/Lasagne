60d4e1b7efe03373bc2e0c8d8141a62a0d11d145
Geoffrey French <frondit1985@gmail.com>
2015-08-13 10:00:18 +0100

    Moved cropping functionality out of MergeLayer in layers/base.py into MergeCropLayer in layers/merge.py as MergeLayer is as a base class by the recurrent layers. Implemented unit tests for the cropping helper functions in MergeCropLayer.

--- lasagne/layers/merge.py
+++ lasagne/layers/merge.py
@@ -11,7 +11,97 @@ __all__ = [
 ]
 
 
-class ConcatLayer(MergeLayer):
+class MergeCropLayer(MergeLayer):
+    """
+    This class adds cropping to MergeLayer.
+    """
+    CROP_NONE = None
+    CROP_LOWER = 'lower'
+    CROP_CENTER = 'center'
+    CROP_UPPER = 'upper'
+
+    def __init__(self, incomings, cropping=None, name=None):
+        super(MergeCropLayer, self).__init__(incomings, name=name)
+
+        self.cropping = cropping
+
+    def _merge_input_shapes(self, input_shapes):
+        if self.cropping is None:
+            return input_shapes[0]
+        else:
+            result = []
+            for sh, cr in zip(zip(*input_shapes), self.cropping):
+                if cr is None:
+                    result.append(sh[0])
+                elif cr in {MergeCropLayer.CROP_LOWER,
+                            MergeCropLayer.CROP_CENTER,
+                            MergeCropLayer.CROP_UPPER}:
+                    result.append(min(sh))
+                else:
+                    raise ValueError('Unknown crop mode \'{0}\''.format(cr))
+            return tuple(result)
+
+    def _crop_inputs(self, inputs):
+        if self.cropping is None:
+            # No cropping in any dimension
+            return inputs
+        else:
+            # Get the number of dimensions
+            ndim = inputs[0].ndim
+            # Get the shape of each input, where each shape will be a Theano
+            # expression
+            shapes = [input.shape for input in inputs]
+            # Convert the shapes to a matrix expression
+            shapes_tensor = T.as_tensor_variable(shapes)
+            # Min along axis 0 to get the minimum size in each dimension
+            min_shape = T.min(shapes_tensor, axis=0)
+
+            # Nested list of slices; each list in `slices` corresponds to
+            # an input and contains a slice for each dimension
+            slices_by_input = [[] for i in range(ndim)]
+
+            # If there are more dimensions than cropping entries, pad
+            # the cropping
+            cropping = list(self.cropping)
+            if ndim > len(cropping):
+                cropping = list(cropping) + \
+                             [None] * (ndim - len(cropping))
+
+            # For each dimension
+            for dim, cr in enumerate(cropping):
+                if cr == MergeCropLayer.CROP_NONE:
+                    # Don't crop this dimension
+                    slice_all = slice(None)
+                    for slices in slices_by_input:
+                        slices.append(slice_all)
+                else:
+                    # We crop all inputs in the dimension `dim` so that they
+                    # are the minimum found in this dimension from all inputs
+                    sz = min_shape[dim]
+                    if cr == MergeCropLayer.CROP_LOWER:
+                        # Choose the first `sz` elements
+                        slc_lower = slice(None, sz)
+                        for slices in slices_by_input:
+                            slices.append(slc_lower)
+                    elif cr == MergeCropLayer.CROP_UPPER:
+                        # Choose the last `sz` elements
+                        slc_upper = slice(-sz, None)
+                        for slices in slices_by_input:
+                            slices.append(slc_upper)
+                    elif cr == MergeCropLayer.CROP_CENTER:
+                        # Choose `sz` elements from the center
+                        for sh, slices in zip(shapes, slices_by_input):
+                            offset = (sh[dim] - sz) // 2
+                            slices.append(slice(offset, offset+sz))
+                    else:
+                        raise ValueError(
+                            'Unknown crop mode \'{0}\''.format(cr))
+
+            return [input[slices] for input, slices in
+                    zip(inputs, slices_by_input)]
+
+
+class ConcatLayer(MergeCropLayer):
     """
     Concatenates multiple inputs along the specified axis. Inputs should have
     the same shape except for the dimension specified in axis, which can have
@@ -25,8 +115,9 @@ class ConcatLayer(MergeLayer):
     axis : int
         Axis which inputs are joined over
     """
-    def __init__(self, incomings, axis=1, **kwargs):
-        super(ConcatLayer, self).__init__(incomings, **kwargs)
+    def __init__(self, incomings, axis=1, cropping=None, **kwargs):
+        super(ConcatLayer, self).__init__(incomings, cropping=cropping,
+                                          **kwargs)
         self.axis = axis
 
     def get_output_shape_for(self, input_shapes):
@@ -41,7 +132,7 @@ class ConcatLayer(MergeLayer):
 concat = ConcatLayer  # shortcut
 
 
-class ElemwiseMergeLayer(MergeLayer):
+class ElemwiseMergeLayer(MergeCropLayer):
     """
     This layer performs an elementwise merge of its input layers.
     It requires all input layers to have the same output shape.
@@ -62,8 +153,9 @@ class ElemwiseMergeLayer(MergeLayer):
     ElemwiseSumLayer : Shortcut for sum layer.
     """
 
-    def __init__(self, incomings, merge_function, **kwargs):
-        super(ElemwiseMergeLayer, self).__init__(incomings, **kwargs)
+    def __init__(self, incomings, merge_function, cropping=None, **kwargs):
+        super(ElemwiseMergeLayer, self).__init__(incomings, cropping=cropping,
+                                                 **kwargs)
         self.merge_function = merge_function
 
     def get_output_shape_for(self, input_shapes):
@@ -105,8 +197,9 @@ class ElemwiseSumLayer(ElemwiseMergeLayer):
     of the same number of output units and add them up afterwards. (This avoids
     the copy operations in concatenation, but splits up the dot product.)
     """
-    def __init__(self, incomings, coeffs=1, **kwargs):
-        super(ElemwiseSumLayer, self).__init__(incomings, T.add, **kwargs)
+    def __init__(self, incomings, coeffs=1, cropping=None, **kwargs):
+        super(ElemwiseSumLayer, self).__init__(incomings, T.add,
+                                               cropping=cropping, **kwargs)
         if isinstance(coeffs, list):
             if len(coeffs) != len(incomings):
                 raise ValueError("Mismatch: got %d coeffs for %d incomings" %
--- lasagne/tests/layers/test_merge.py
+++ lasagne/tests/layers/test_merge.py
@@ -4,6 +4,98 @@ import pytest
 import theano
 
 
+class TestMergeCropLayer:
+    # Test internal helper methods of MergeCropLayer
+    def test_merge_shapes(self):
+        from lasagne.layers.merge import MergeCropLayer
+        crop0 = MergeCropLayer([], cropping=None)
+        crop1 = MergeCropLayer([], cropping=[
+            MergeCropLayer.CROP_NONE,
+            MergeCropLayer.CROP_LOWER,
+            MergeCropLayer.CROP_CENTER,
+            MergeCropLayer.CROP_UPPER
+        ])
+
+        assert crop0._merge_input_shapes(
+            [(1, 2, 3, 4), (5, 6, 7, 8), (5, 4, 3, 2)]) == (1, 2, 3, 4)
+        assert crop1._merge_input_shapes(
+            [(1, 2, 3, 4), (5, 6, 7, 8), (5, 4, 3, 2)]) == (1, 2, 3, 2)
+
+    def test_crop_inputs(self):
+        from lasagne.layers.merge import MergeCropLayer
+        from numpy.testing import assert_array_equal
+        crop_0 = MergeCropLayer([], cropping=None)
+        crop_1 = MergeCropLayer([], cropping=[
+            MergeCropLayer.CROP_NONE,
+            MergeCropLayer.CROP_LOWER,
+            MergeCropLayer.CROP_CENTER,
+            MergeCropLayer.CROP_UPPER
+        ])
+        crop_l = MergeCropLayer([], cropping=[
+            MergeCropLayer.CROP_LOWER,
+            MergeCropLayer.CROP_LOWER,
+            MergeCropLayer.CROP_LOWER,
+            MergeCropLayer.CROP_LOWER
+        ])
+        crop_c = MergeCropLayer([], cropping=[
+            MergeCropLayer.CROP_CENTER,
+            MergeCropLayer.CROP_CENTER,
+            MergeCropLayer.CROP_CENTER,
+            MergeCropLayer.CROP_CENTER
+        ])
+        crop_u = MergeCropLayer([], cropping=[
+            MergeCropLayer.CROP_UPPER,
+            MergeCropLayer.CROP_UPPER,
+            MergeCropLayer.CROP_UPPER,
+            MergeCropLayer.CROP_UPPER
+        ])
+
+        x0 = numpy.random.random((2, 3, 5, 7))
+        x1 = numpy.random.random((1, 2, 3, 4))
+        x2 = numpy.random.random((6, 3, 4, 2))
+
+        def crop_test(cr, inputs, expected):
+            inputs = [theano.shared(x) for x in inputs]
+            outs = cr._crop_inputs(inputs)
+            outs = [o.eval() for o in outs]
+            assert len(outs) == len(expected)
+            for o, e in zip(outs, expected):
+                assert_array_equal(o, e)
+
+        crop_test(crop_0, [x0, x1],
+                  [x0, x1])
+        crop_test(crop_1, [x0, x1],
+                  [x0[:, :2, 1:4, 3:], x1[:, :, :, :]])
+        crop_test(crop_l, [x0, x1],
+                  [x0[:1, :2, :3, :4], x1[:, :, :, :]])
+        crop_test(crop_c, [x0, x1],
+                  [x0[:1, :2, 1:4, 1:5], x1[:, :, :, :]])
+        crop_test(crop_u, [x0, x1],
+                  [x0[1:, 1:, 2:, 3:], x1[:, :, :, :]])
+
+        crop_test(crop_0, [x0, x2],
+                  [x0, x2])
+        crop_test(crop_1, [x0, x2],
+                  [x0[:, :, :4, 5:], x2[:, :, :, :]])
+        crop_test(crop_l, [x0, x2],
+                  [x0[:, :, :4, :2], x2[:2, :, :, :]])
+        crop_test(crop_c, [x0, x2],
+                  [x0[:, :, :4, 2:4], x2[2:4, :, :, :]])
+        crop_test(crop_u, [x0, x2],
+                  [x0[:, :, 1:, 5:], x2[4:, :, :, :]])
+
+        crop_test(crop_0, [x0, x1, x2],
+                  [x0, x1, x2])
+        crop_test(crop_1, [x0, x1, x2],
+                  [x0[:, :2, 1:4, 5:], x1[:, :, :, 2:], x2[:, :2, :3, :]])
+        crop_test(crop_l, [x0, x1, x2],
+                  [x0[:1, :2, :3, :2], x1[:, :, :, :2], x2[:1, :2, :3, :]])
+        crop_test(crop_c, [x0, x1, x2],
+                  [x0[:1, :2, 1:4, 2:4], x1[:, :, :, 1:3], x2[2:3, :2, :3, :]])
+        crop_test(crop_u, [x0, x1, x2],
+                  [x0[1:, 1:, 2:, 5:], x1[:, :, :, 2:], x2[5:, 1:, 1:, :]])
+
+
 class TestConcatLayer:
     @pytest.fixture
     def layer(self):
